5cfb680e9f7128c2178fb7d8f98f01bacc0729dc
arthurpalomar <arthurpalomar24@gmail.com>
2024-07-27 00:13:19 -0400

    fix: correct link and vercel build logic

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,22 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
+import { globSync } from 'fast-glob';
+
+const htmlFiles = globSync('src/**/*.html').reduce((acc, file) => {
+  const name = file.replace(/src\/|\.html/g, '').replace(/\//g, '_');
+  acc[name] = resolve(__dirname, file);
+  return acc;
+}, {});
 
 export default defineConfig({
+  root: 'src',
   build: {
-    outDir: 'dist',
+    rollupOptions: {
+      input: htmlFiles,
+      output: {
+        dir: resolve(__dirname, 'dist'),
+      },
+    },
   },
-})
+  publicDir: 'public',
+});
